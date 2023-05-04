27616ba655700c97a1c66cc9dc130ae5b9a16d35
Keks <keks@htmlacademy.ru>
2023-05-05 01:28:48 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/menuuu.js
@@ -0,0 +1,12 @@
+let navMain = document.querySelector('.header');
+let navToggle = document.querySelector('.header__toggle');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('header--closed')) {
+    navMain.classList.remove('header--closed');
+    navMain.classList.add('header--opened');
+  } else {
+    navMain.classList.add('header--closed');
+    navMain.classList.remove('header--opened');
+  }
+});
